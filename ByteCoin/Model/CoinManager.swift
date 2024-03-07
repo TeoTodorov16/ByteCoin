8196b9e48b1bc958955bc3a65880c8f1c81b1b84
Teodor <teodorttodorov@icloud.com>
2024-03-07 18:00:28 +0200

    Adding new method in the CoinManager and then reassigning the value of it Selected Row to the CoinManager's method by wrapping it in a constant variable

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -13,6 +13,10 @@ struct CoinManager {
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "YOUR_API_KEY_HERE"
     
+    func getCoinPrice(for currency: String) {
+        
+    }
+    
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     
