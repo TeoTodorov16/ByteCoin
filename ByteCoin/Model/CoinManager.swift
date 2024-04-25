8815e0a3a80448fc7a106e737b03ba89cdb0fcff
Teodor <teodorttodorov@icloud.com>
2024-04-25 12:12:16 +0300

    Created a URL string with Session & Task.

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -12,36 +12,34 @@ struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "YOUR_API_KEY_HERE"
-   
-    
-    
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
     func getCoinPrice(for currency: String) {
+        
+        //Use String concatenation to add the selected currency at the end of the baseURL along with the API key.
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        //Use optional binding to unwrap the URL that's created from the urlString
+        if let url = URL(string: urlString) {
             
-            //Use String concatenation to add the selected currency at the end of the baseURL along with the API key.
-            let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+            //Create a new URLSession object with default configuration.
+            let session = URLSession(configuration: .default)
             
-            //Use optional binding to unwrap the URL that's created from the urlString
-            if let url = URL(string: urlString) {
-                
-                //Create a new URLSession object with default configuration.
-                let session = URLSession(configuration: .default)
-                
-                //Create a new data task for the URLSession
-                let task = session.dataTask(with: url) { (data, response, error) in
-                    if error != nil {
-                        print(error!)
-                        return
-                    }
-                    //Format the data we got back as a string to be able to print it.
-                    let dataAsString = String(data: data!, encoding: .utf8)
-                    print(dataAsString)
-                    
+            //Create a new data task for the URLSession
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
                 }
-                //Start task to fetch data from bitcoin average's servers.
-                task.resume()
+                //Format the data we got back as a string to be able to print it.
+                let dataAsString = String(data: data!, encoding: .utf8)
+                print(dataAsString)
+                
             }
+            //Start task to fetch data from bitcoin average's servers.
+            task.resume()
         }
-    // Making a new change to display the latest price of 1 Bitcoin (BTC) in US dollars (USD).
+    }
+    
 }
