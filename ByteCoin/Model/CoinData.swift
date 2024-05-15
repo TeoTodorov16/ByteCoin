e343fada4958ded4609dc062a08b4cc3a3cd3818
Teodor <teodorttodorov@icloud.com>
2024-05-15 14:43:31 +0300

    Creating a CoinData file to decode the Bitcoin data.

--- ByteCoin/Model/CoinData.swift
+++ ByteCoin/Model/CoinData.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
-struct CoinData: Codable {
-    let name: 
+struct CoinData: Decodable {
+    let time: String
+    let asset_id_base: String
+    let asset_id_quote: String
+    let rate: Double
 }
