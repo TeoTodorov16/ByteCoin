2e31872735f5c74918d497766c47003d3c9d165a
Teodor <teodorttodorov@icloud.com>
2024-05-16 15:10:07 +0300

    commit

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -25,20 +25,25 @@ struct CoinManager {
     func getCoinPrice(for currency: String) {
         
         //Use String concatenation to add the selected currency at the end of the baseURL along with the API key.
-        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
-        
-        //Use optional binding to unwrap the URL that's created from the urlString
-        if let url = URL(string: urlString) {
-            
-            //Create a new URLSession object with default configuration.
-            let session = URLSession(configuration: .default)
-            
-            //Create a new data task for the URLSession
-            let task = session.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    print(error!)
-                    return
-                }
+        var delegate: CoinManagerDelegate?
+           
+           let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+           let apiKey = "YOUR_API_KEY_HERE"
+
+           let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+           
+           func getCoinPrice(for currency: String) {
+               
+               let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+
+               if let url = URL(string: urlString) {
+                   
+                   let session = URLSession(configuration: .default)
+                   let task = session.dataTask(with: url) { (data, response, error) in
+                       if error != nil {
+                           self.delegate?.didFailWithError(error: error!)
+                           return
+                       }
                
                 if let safeData = data {
                     let bitcoinPrice = self.parseJSON(safeData)
