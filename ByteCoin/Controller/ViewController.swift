42da19bf7339c9332135dc63568405539a13ef16
Teodor <teodorttodorov@icloud.com>
2024-05-16 15:15:38 +0300

    commit

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -26,23 +26,21 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
             currencyPicker.dataSource = self
             currencyPicker.delegate = self
             
+        }//Provide the implementation for the delegate methods.
+    
+    //When the coinManager gets the price it will call this method and pass over the price and currency.
+    func didUpdatePrice(price: String, currency: String) {
+        
+        //Remember that we need to get hold of the main thread to update the UI, otherwise our app will crash if we
+        //try to do this from a background thread (URLSession works in the background).
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
         }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return coinManager.currencyArray.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return coinManager.currencyArray[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        let selectedCurrency = coinManager.currencyArray[row]
-        coinManager.getCoinPrice(for: selectedCurrency)
+    func didFailWithError(error: Error) {
+        print(error)
     }
     
     
