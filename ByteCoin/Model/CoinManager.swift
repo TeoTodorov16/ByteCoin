298fcd86efafed97eda2f42c74e3858e4382d4d6
Teodor <teodorttodorov@icloud.com>
2024-05-16 15:12:49 +0300

    finishing the delegate & protocol connection in CoinManager

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+//By convention, Swift protocols are usually written in the file that has the class/struct which will call the
+//delegate methods, i.e. the CoinManager.
+protocol CoinManagerDelegate {
+    
+    
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
     //Create an optional delegate that will have to implement the delegate methods.
@@ -50,30 +59,18 @@ struct CoinManager {
     }
     
     func parseJSON(_ data: Data) -> Double? {
+        
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            print(lastPrice)
+            return lastPrice
             
-            //Create a JSONDecoder
-            let decoder = JSONDecoder()
-            do {
-                
-                //try to decode the data using the CoinData structure
-                let decodedData = try decoder.decode(CoinData.self, from: data)
-                
-                //Get the last property from the decoded data.
-                let lastPrice = decodedData.rate
-                print(lastPrice)
-                return lastPrice
-                
-                
-                
-            } catch {
-                
-                //Catch and print any errors.
-                print(error)
-                return nil
-            }
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
         }
+    }
     
 }
-
-//apikey - 49E6AF99-A3AB-4B4B-97B3-91E443B71922
-
